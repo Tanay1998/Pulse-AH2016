bfdfa51f8ef85aa6376bb91c98e3523752a4d837
ananay <i@ananayarora.com>
2016-03-24 18:03:51 +0530

    Added Tabs with fragment for Frequents.

--- mobile/src/main/java/com/threadlyapp/threadly/home_screen.java
+++ mobile/src/main/java/com/threadlyapp/threadly/home_screen.java
@@ -3,11 +3,16 @@ package com.threadlyapp.threadly;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
-
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 public class home_screen extends AppCompatActivity {
 
     @Override
@@ -25,6 +30,26 @@ public class home_screen extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.titleA, home_screenFragment.class)
+                .add(R.string.titleB, frequents.class)
+                .add(R.string.titleC, home_screenFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
+        viewPagerTab.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Snackbar.make(v, "Arrey! LOL abhi kuch nahi hota.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return true;
+            }
+        });
     }
+
 
 }
